0fe923bf1405bc3ca2fcaa128050c38e72857ab1
piper <2370344854@qq.com>
2021-07-08 08:42:18 +0800

    feat:db

---
+++ spring-server/src/test/java/im/piper/spring_server/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package im.piper.spring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
