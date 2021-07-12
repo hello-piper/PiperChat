4721d598640a44daafc22b9c737eca0c0633e986
piper <2370344854@qq.com>
2021-07-12 08:42:20 +0800

    feat:client

---
+++ client/src/main/java/piper/im/client/MultipleWebSocketClient.java
@@ -0,0 +1,105 @@
+package piper.im.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import piper.im.common.util.ThreadUtil;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+public class MultipleWebSocketClient {
+
+    static final String URL = System.getProperty("url", "ws://127.0.0.1:8080/websocket");
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 1; i++) {
+            Channel one = getOne();
+            if (one != null) {
+                ThreadUtil.SCHEDULE_POOL.scheduleWithFixedDelay(() ->
+                        one.writeAndFlush(new TextWebSocketFrame("hello")), 1, 20, TimeUnit.SECONDS);
+            }
+        }
+        Thread.sleep(100000000000L);
+    }
+
+    public static Channel getOne() throws Exception {
+        URI uri = new URI(URL);
+        String scheme = uri.getScheme() == null ? "ws" : uri.getScheme();
+        final String host = uri.getHost() == null ? "127.0.0.1" : uri.getHost();
+        final int port;
+        if (uri.getPort() == -1) {
+            if ("ws".equalsIgnoreCase(scheme)) {
+                port = 80;
+            } else if ("wss".equalsIgnoreCase(scheme)) {
+                port = 443;
+            } else {
+                port = -1;
+            }
+        } else {
+            port = uri.getPort();
+        }
+
+        if (!"ws".equalsIgnoreCase(scheme) && !"wss".equalsIgnoreCase(scheme)) {
+            System.err.println("Only WS(S) is supported.");
+            return null;
+        }
+
+        final boolean ssl = "wss".equalsIgnoreCase(scheme);
+        final SslContext sslCtx;
+        if (ssl) {
+            sslCtx = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        } else {
+            sslCtx = null;
+        }
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            final WebSocketClientHandler handler =
+                    new WebSocketClientHandler(
+                            WebSocketClientHandshakerFactory.newHandshaker(
+                                    uri, WebSocketVersion.V13, null, true, new DefaultHttpHeaders()));
+
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            if (sslCtx != null) {
+                                p.addLast(sslCtx.newHandler(ch.alloc(), host, port));
+                            }
+                            p.addLast(
+                                    new HttpClientCodec(),
+                                    new HttpObjectAggregator(8192),
+                                    WebSocketClientCompressionHandler.INSTANCE,
+                                    handler);
+                        }
+                    });
+
+            Channel ch = b.connect(uri.getHost(), uri.getPort()).sync().channel();
+            handler.handshakeFuture().sync();
+            return ch;
+            // todo
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
