8c3cff5e748ca5b583f573f1ff885bc3ebce6463
piper <2370344854@qq.com>
2021-08-11 23:01:28 +0800

    feat:cmd

---
+++ common/src/main/java/io/piper/common/enums/CmdTypeEnum.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020-2030 The Piper(https://github.com/hello-piper)
+ *
+ * The PiperChat is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ * http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+package io.piper.common.enums;
+
+import java.util.Arrays;
+
+/**
+ * 信令类型枚举
+ *
+ * @author piper
+ */
+public enum CmdTypeEnum {
+    HEARTBEAT((byte) 0),
+    SUB_ROOM((byte) 1);
+
+    public Byte type;
+
+    CmdTypeEnum(Byte type) {
+        this.type = type;
+    }
+
+    public static CmdTypeEnum valueOf(Byte type) {
+        return Arrays.stream(values()).filter(v -> v.type.equals(type)).findFirst().orElse(null);
+    }
+}
